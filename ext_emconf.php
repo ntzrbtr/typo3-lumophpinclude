e0eec7bcd58069d168a1e46717df4098b2986cc3
Thomas Off <retiolum@googlemail.com>
2014-06-09 16:51:20 +0200

    Update hashes of files.

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,14 +3,14 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "lumophpinclude".
  *
- * Auto generated 02-05-2014 14:27
+ * Auto generated 09-06-2014 16:51
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'LumoNet PHP Include',
 	'description' => 'Includes local PHP scripts via eval() or external PHP script via HTTP. Provides options to transfer GET and POST variables to the included script and strip header and footer HTML parts.',
 	'category' => 'plugin',
@@ -20,8 +20,6 @@ $EM_CONF[$_EXTKEY] = array (
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '4.0.0-4.7.99',
-	'PHP_version' => '4.4.0-5.5.99',
 	'module' => '',
 	'state' => 'stable',
 	'uploadfolder' => 1,
@@ -32,8 +30,19 @@ $EM_CONF[$_EXTKEY] = array (
 	'author' => 'Thomas Off',
 	'author_email' => 'retiolum@googlemail.com',
 	'author_company' => '',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '4.4.0-5.5.99',
+			'typo3' => '4.0.0-4.7.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"1299";s:12:"ext_icon.gif";s:4:"e86e";s:17:"ext_localconf.php";s:4:"294e";s:14:"ext_tables.php";s:4:"5b1c";s:13:"locallang.xml";s:4:"f7b9";s:16:"locallang_db.xml";s:4:"8e64";s:10:"README.txt";s:4:"7559";s:14:"doc/manual.sxw";s:4:"d7ba";s:20:"lib/Snoopy.class.php";s:4:"cb7a";s:14:"pi1/ce_wiz.gif";s:4:"4012";s:35:"pi1/class.tx_lumophpinclude_pi1.php";s:4:"c5a7";s:43:"pi1/class.tx_lumophpinclude_pi1_wizicon.php";s:4:"4871";s:13:"pi1/clear.gif";s:4:"cc11";s:23:"pi1/flexform_ds_pi1.xml";s:4:"8ead";s:17:"pi1/locallang.xml";s:4:"8c9b";}',
 );
 
 ?>
