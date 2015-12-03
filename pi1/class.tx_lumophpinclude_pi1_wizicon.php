2a25631b690b06c5e2d4a9312f82d79dd180ad91
Thomas Off <retiolum@googlemail.com>
2015-12-03 22:56:54 +0100

    Only use namespaced code from the TYPO3 core.

--- pi1/class.tx_lumophpinclude_pi1_wizicon.php
+++ pi1/class.tx_lumophpinclude_pi1_wizicon.php
@@ -1,72 +1,67 @@
 <?php
-/*
- * Copyright notice
+
+/***************************************************************
+ *  Copyright notice
  *
- * (c) 2005-2008 Thomas Off <typo3@retiolum.de>
- * All rights reserved
+ *  (c) 2015 Thomas Off <retiolum@googlemail.com>
+ *  All rights reserved
  *
- * This script is part of the TYPO3 project. The TYPO3 project is
- * free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
  *
- * The GNU General Public License can be found at
- * http://www.gnu.org/copyleft/gpl.html.
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
  *
- * This script is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * This copyright notice MUST APPEAR in all copies of the script!
- */
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
  * Class that adds the wizard icon.
  *
- * @author Thomas Off <typo3@retiolum.de>
- * @package TYPO3
- * @subpackage tx_lumogooglemaps
+ * @package lumonetphpinclude
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  */
 class tx_lumophpinclude_pi1_wizicon {
 
-    /**
-     * Function to add item in the wizard for creating new content elements.
-     *
-     * @param array Array with wizard items added so far
-     * @return array Array of wizard items with entry for this extension added
-     */
-    function proc($wizardItems) {
-        global $LANG;
+	/**
+	 * Function to add item in the wizard for creating new content elements.
+	 *
+	 * @param array Array with wizard items added so far
+	 * @return array Array of wizard items with entry for this extension added
+	 */
+	function proc($wizardItems) {
+		global $LANG;
 
-        $LL = $this->includeLocalLang();
+		$LL = $this->includeLocalLang();
 
-        $wizardItems['plugins_tx_lumophpinclude_pi1'] = array(
-            'icon' => t3lib_extMgm::extRelPath('lumophpinclude') . 'pi1/ce_wiz.gif',
-            'title' => $LANG->getLLL('pi1_title', $LL),
-            'description' => $LANG->getLLL('pi1_plus_wiz_description', $LL),
-            'params' => '&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=lumophpinclude_pi1'
-            );
+		$wizardItems['plugins_tx_lumophpinclude_pi1'] = array(
+			'icon' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('lumophpinclude') . 'pi1/ce_wiz.gif',
+			'title' => $LANG->getLLL('pi1_title', $LL),
+			'description' => $LANG->getLLL('pi1_plus_wiz_description', $LL),
+			'params' => '&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=lumophpinclude_pi1',
+		);
 
-        return $wizardItems;
-    }
+		return $wizardItems;
+	}
 
-    /**
-     * Function to include language files.
-     *
-     * @return array Array containing language labels
-     */
-    function includeLocalLang() {
-        $llFile = t3lib_extMgm::extPath('lumophpinclude') . 'locallang.xml';
-        $LOCAL_LANG = t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
-        return $LOCAL_LANG;
-    }
+	/**
+	 * Function to include language files.
+	 *
+	 * @return array Array containing language labels
+	 */
+	function includeLocalLang() {
+		$llFile = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('lumophpinclude') . 'locallang.xml';
+		$LOCAL_LANG = \TYPO3\CMS\Core\Utility\GeneralUtility::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
+
+		return $LOCAL_LANG;
+	}
 
 }
-
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/lumophpinclude/pi1/class.tx_lumophpinclude_pi1_wizicon.php']) {
-    include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/lumophpinclude/pi1/class.tx_lumophpinclude_pi1_wizicon.php']);
-}
-
-?>
