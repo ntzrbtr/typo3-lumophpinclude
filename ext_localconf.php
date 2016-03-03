3d8ee4184ab6280ab499be4905a6320e58aaa7a4
Thomas Off <retiolum@googlemail.com>
2016-03-03 23:06:06 +0100

    Use a boot function as it seems to be good practice and state-of-the-art right now.

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,16 +3,22 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-// Add the frontend plugin render definition.
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-	$_EXTKEY . '_pi1',
-	'setup',
-	'
-plugin.tx_lumophpinclude_pi1 = USER
-plugin.tx_lumophpinclude_pi1 {
+$boot = function($_EXTKEY) {
+	// Add the frontend plugin render definition.
+	$typoScript = <<<EOT
+plugin.tx_{$_EXTKEY}_pi1 = USER
+plugin.tx_{$_EXTKEY}_pi1 {
 	userFunc = Retiolum\Lumophpinclude\Pi1\Plugin->main
 }
 
 tt_content.list.20.lumophpinclude_pi1 =< plugin.tx_lumophpinclude_pi1
-	',
-	'defaultContentRendering');
+EOT;
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+		$_EXTKEY . '_pi1',
+		'setup',
+		$typoScript,
+		'defaultContentRendering'
+	);
+};
+$boot($_EXTKEY);
+unset($boot);
--- ext_tables.php
+++ ext_tables.php
@@ -3,18 +3,22 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-// Add the frontend plugin.
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-	array(
-    	'LLL:EXT:lumophpinclude/Resources/Private/Language/locallang_be.xlf:pi1_title',
-		$_EXTKEY . '_pi1'
-	),
-	'list_type'
-);
+$boot = function($_EXTKEY) {
+	// Add the frontend plugin.
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+		array(
+			'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_be.xlf:pi1_title',
+			$_EXTKEY . '_pi1'
+		),
+		'list_type'
+	);
 
-// Use FlexForms for the plugin.
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:lumophpinclude/Configuration/FlexForms/flexform_pi1.xml');
+	// Use FlexForms for the plugin.
+	$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_pi1.xml');
 
-// Define icon for the wizard.
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:lumophpinclude/Configuration/TSconfig/PageTS.txt">');
+	// Define icon for the wizard.
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/PageTS.txt">');
+};
+$boot($_EXTKEY);
+unset($boot);
